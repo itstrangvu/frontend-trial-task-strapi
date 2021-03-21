2f990719b81d7acce361951073167529ecf7dec9
Trang Vu <vu@trang.home>
2021-03-21 23:48:20 +0100

    Add all files

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6a8cbc5210441026aae5240f909b34dd'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '95f8034a-c265-455e-beb4-7cc3f0874de6'
+};
